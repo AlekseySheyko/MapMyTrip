9ba0870071694eb833914c9e6fa6ad32d69fbd05
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-01-22 13:36:40 +0200

    Заебашил распарс строки.

--- app/src/main/java/sheyko/aleksey/mapthetrip/ui/adapters/StatsListAdapter.java
+++ app/src/main/java/sheyko/aleksey/mapthetrip/ui/adapters/StatsListAdapter.java
@@ -13,10 +13,19 @@ public class StatsListAdapter extends ArrayAdapter<String> {
     private final Context context;
     private final String[] values;
 
+    private String[] separatedCodes;
+    private String[] separatedDistances;
+
     public StatsListAdapter(Context context, String[] values) {
         super(context, R.layout.stats_list_item, values);
         this.context = context;
         this.values = values;
+
+        String stateCodes = values[0];
+        String stateDistances = values[1];
+
+        separatedCodes = stateCodes.split(",");
+        separatedDistances = stateDistances.split(",");
     }
 
     @Override
@@ -26,10 +35,16 @@ public class StatsListAdapter extends ArrayAdapter<String> {
         View rowView = inflater.inflate(R.layout.stats_list_item, parent, false);
 
         TextView stateCodeTextView = (TextView) rowView.findViewById(R.id.stateCodeLabel);
-        stateCodeTextView.setText(values[0]);
+        stateCodeTextView.setText(
+                String.format("%.2f",
+                        Float.parseFloat(
+                                separatedCodes[position])));
 
         TextView stateDistanceTextView = (TextView) rowView.findViewById(R.id.stateDistanceLabel);
-        stateDistanceTextView.setText(values[1]);
+        stateDistanceTextView.setText(
+                String.format("%.2f",
+                        Float.parseFloat(
+                                separatedDistances[position].trim())));
 
         return rowView;
     }
